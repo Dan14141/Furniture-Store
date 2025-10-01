1062d9b6e469fc7c884436961b39799c699334dc
Dan <dan.sim2014@yandex.ru>
2025-10-01 22:03:27 +0300

    commit pagination second through GET

--- goods/urls.py
+++ goods/urls.py
@@ -4,6 +4,5 @@ from goods import views as goods_views
 app_name = 'goods'
 urlpatterns = [
     path('<slug:category_slug>/',goods_views.catalog,name='index'),
-    path('<slug:category_slug>/<int:page>/',goods_views.catalog,name='index'),
     path('product/<slug:product_slug>/',goods_views.product,name='product'),
 ]
